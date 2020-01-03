eba168a090293325453344e979d9f6deef647e39
songjunbao <songjunbao@bx.cn>
2020-01-04 00:05:31 +0800

    事件机制 和 动态组装bean

--- src/main/java/com/kinbo/boot2deep/controller/EchoController.java
+++ src/main/java/com/kinbo/boot2deep/controller/EchoController.java
@@ -1,6 +1,8 @@
 package com.kinbo.boot2deep.controller;
 
 import com.kinbo.boot2deep.service.EchoService;
+import com.kinbo.boot2deep.service.check.CompositeCheckService;
+import com.kinbo.boot2deep.service.event.ConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,20 @@ public class EchoController {
     @Autowired
     private EchoService echoService;
 
+    @Autowired
+    private CompositeCheckService checkService;
+
+    @Autowired
+    private ConfigService configService;
 
     @RequestMapping("/echo/{message}")
-    public String echo(@PathVariable String message){
+    public String echo(@PathVariable String message) {
+        try {
+
+            Thread.sleep(50000L);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
 
         return echoService.echo(message);
     }
@@ -27,4 +40,14 @@ public class EchoController {
         return echoService.echo(message);
     }
 
+    @RequestMapping("/check")
+    public String check(){
+        checkService.check();
+        return "success";
+    }
+
+    @RequestMapping("/configChange")
+    public void configChange(){
+        configService.subscribeChange();
+    }
 }
